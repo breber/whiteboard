db955ae17774499fd13503b3caa4ce12e8ba5d2d
Brian Reber <reber.brian@gmail.com>
2012-09-06 11:29:40 -0500

    Allow users to disconnect from Dropbox (2)
    Add the button on the PreferenceActivity that doesn't use
    PreferenceFragments.

--- whiteboard/src/com/brianreber/whiteboard/WhiteboardPreferenceActivitySupport.java
+++ whiteboard/src/com/brianreber/whiteboard/WhiteboardPreferenceActivitySupport.java
@@ -2,16 +2,56 @@ package com.brianreber.whiteboard;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 
+/**
+ * Application preferences for devices which do not support PreferenceFragments
+ * 
+ * @author breber
+ */
 public class WhiteboardPreferenceActivitySupport extends PreferenceActivity {
+
+	/**
+	 * The Disconnect from Dropbox button
+	 */
+	private Button disconnectDropbox;
 
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		addPreferencesFromResource(R.xml.preferences);
 
+		ListView lv = getListView();
+
+		disconnectDropbox = new Button(this);
+		disconnectDropbox.setText(R.string.unlinkDropbox);
+		disconnectDropbox.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				DropboxUtils.clearAuthentication(WhiteboardPreferenceActivitySupport.this);
+				enableDisableButton();
+			}
+		});
+
+		enableDisableButton();
+		lv.addHeaderView(disconnectDropbox);
 	}
 
+	/**
+	 * Enable or disable the Unlink button
+	 */
+	private void enableDisableButton() {
+		if (disconnectDropbox != null) {
+			// Enable the button when we are linked, disable when we aren't
+			if (DropboxUtils.isLinked(this)) {
+				disconnectDropbox.setEnabled(true);
+			} else {
+				disconnectDropbox.setEnabled(false);
+			}
+		}
+	}
 }
